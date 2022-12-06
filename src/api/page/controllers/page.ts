2576640df14ebe2749237b1cfea4dc4d05be660b
Kalaiarasan Ayyanar <kalaiarasan.ayyanar@gmail.com>
2022-12-06 21:16:50 +0000

    chore: sample custom logic added to controller

--- src/api/page/controllers/page.ts
+++ src/api/page/controllers/page.ts
@@ -2,6 +2,17 @@
  * page controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::page.page');
+export default factories.createCoreController(
+  "api::page.page",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const result = await super.find(ctx);
+
+      result.meta.date = Date.now();
+
+      return result;
+    },
+  })
+);
